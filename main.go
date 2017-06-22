f1e09978edcf514a07458a6ccf4458b334ec518d
Florian Uekermann <florian@uekermann.me>
2017-06-22 13:08:15 +0200

    Switch to basic auth and try reading password from enviroment

--- main.go
+++ main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Generate secret path with 144 bits of entropy
-	var entropy [18]byte
-	fmt.Println("Generating secret path...")
-	if _, err := rand.Read(entropy[:]); err != nil {
-		log.Fatalln("Could not generate path:", err.Error())
+	// Read password from enviroment
+	const env = "VOID_DISKEXPLORER_PASSWORD"
+	password := os.Getenv(env)
+
+	// Generate password if it wasn't specified by environment variable
+	if password == "" {
+		fmt.Println(env, "not set. Generating password...")
+		var secret [18]byte
+		if _, err := rand.Read(secret[:]); err != nil {
+			log.Fatalln("Could not generate password:", err.Error())
+		}
+		password = base64.URLEncoding.EncodeToString(secret[:])
 	}
-	path := "/" + base64.URLEncoding.EncodeToString(entropy[:]) + "/"
-	fmt.Printf("The path is: xxx.sourcevoid.net%s", path)
-	// Serve filesystem at secret path
-	http.Handle(path, http.StripPrefix(path, http.FileServer(http.Dir("/"))))
+
+	// Write login details to log
+	fmt.Println("Any user is valid.")
+	fmt.Println("The password is:", password)
+
+	// Authenticate and serve filesystem
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("WWW-Authenticate", `Basic realm="sourcevoid_diskexplorer"`)
+		_, requestPassword, _ := r.BasicAuth()
+		if subtle.ConstantTimeCompare([]byte(password), []byte(requestPassword)) != 1 {
+			http.Error(w, "Enter any user and the password.", http.StatusUnauthorized)
+		} else {
+			http.FileServer(http.Dir("/")).ServeHTTP(w, r)
+		}
+	})
+
+	// Start server
 	log.Fatalln(http.ListenAndServe("localhost:8080", nil))
 }
